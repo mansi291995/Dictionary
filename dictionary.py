704baaa8503ea791a2fc7a7caab0ddefe31b540d
Mansi Ekbote <75264738+mansi291995@users.noreply.github.com>
2021-08-15 12:38:43 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+dict = {
+    "set": "collections of varibles",
+    "object": "collection of functions",
+    "append": "insert element at the end",
+    "print": "print the statement"
+}
+print("enter ur word")
+ax = input()
+#print(dict["ax"])
+print(dict[ax])
+# print("enter ur word")
+# result2 = input()
+# print(dict["object"])
+
+# print("enter ur word")
+# result2 = input()
+# print(dict["object"])
+
+
+
+# print(dict)
